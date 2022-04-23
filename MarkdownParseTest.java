49fca1234ed0fe52e96e518993ddb77a1578b45d
JZ567 <jzwiebel@ucsd.edu>
2022-04-22 21:42:05 -0700

    added test files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,29 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+
+    }
+
+    @Test
+    public void test() throws IOException{
+        assertEquals(2, 1+1);
+        
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(links, List.of("https://something.com", "some-thing.html"));
+        
+        //(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
 }
