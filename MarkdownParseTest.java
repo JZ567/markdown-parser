1ff3409dcb83b2a382ecff64e4d7f28489a77d57
JZ567 <jzwiebel@ucsd.edu>
2022-05-20 17:00:28 -0700

    Added test methods

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,7 +20,7 @@ public class MarkdownParseTest {
 
     }
 
-    /** 
+/**
     @Test
     public void test() throws IOException{
         assertEquals(2, 1+1);
@@ -49,6 +49,37 @@ public class MarkdownParseTest {
         assertEquals(links, List.of("a link on the first line"));
 
     }
+
 */
+    @Test
+    public void testSnippet1() throws IOException{
+        Path fileName = Path.of("snippet-1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(links, List.of("a link on the first line"));
+
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        Path fileName = Path.of("snippet-2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(links, List.of("a link on the first line"));
+
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        Path fileName = Path.of("snippet-3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        assertEquals(links, List.of("a link on the first line"));
+
+    }
+
 }
 
